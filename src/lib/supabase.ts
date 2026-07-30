82bbb1f7b1587b1afdd04fd27d9ef6cba952956b
Eternal <gracious0603@gmail.com>
2026-07-30 19:40:53 +0200

    add map,login,database

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kwnsfdtvlywajuwyibed.supabase.co/rest/v1/';
+const supabaseUrl = 'https://kwnsfdtvlywajuwyibed.supabase.co"';
 const supabaseKey = 'sb_publishable_wSsInPnZIm-x5wmOwT1bbw_07kO1dCJ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
